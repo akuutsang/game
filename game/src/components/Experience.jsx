1dbf5f16fc84d09b0b03ff079ef943a0795545ac
akutsang <akuutsang@gmail.com>
2024-11-29 22:18:26 +0100

    add gates

--- game/src/components/Experience.jsx
+++ game/src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { OrbitControls } from "@react-three/drei";
 import { CylinderCollider, RigidBody } from "@react-three/rapier";
+import { Torii } from "./Torii";
 
 export const Experience = () => {
   return (
@@ -12,6 +13,18 @@ export const Experience = () => {
         castShadow
         color={"#9e69da"}
       />
+      {/* BACKGROUND */}
+      <Torii
+        scale={[16, 16, 16]}
+        position={[0, 0, -22]}
+        rotation-y={1.25 * Math.PI}
+      />
+      <Torii
+        scale={[10, 10, 10]}
+        position={[-8, 0, -20]}
+        rotation-y={1.24 * Math.PI}
+      />
+      <Torii scale={[10, 10, 10]} position={[8, 0, -20]} rotation-y={Math.PI} />
       <RigidBody colliders={false} type="fixed" position-y={-0.5}>
         <CylinderCollider args={[1 / 2, 5]} />
         <mesh scale={[5, 1, 5]} receiveShadow>
